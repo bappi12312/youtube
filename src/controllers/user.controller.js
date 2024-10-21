a129fa7ae55af20ddcc8b93e544a564aca412db3
bappi12312 <ranamdbappihossain@gmail.com>
2024-10-21 23:13:05 +0600

    refresh access token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -153,8 +153,54 @@ const logoutUser = asyncHandler(async (req, res) => {
     )
 })
 
+const refreshAccessToken = asyncHandler(async (req, res) => {
+  const incomingRefreshToken = req.cookies?.refreshToken || req.body.refreshToken;
+
+  if (!incomingRefreshToken) {
+    throw new ApiError(401, "unauthorized request")
+  }
+
+  try {
+    const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET)
+
+    const user = await User.findById(decodedToken?._id)
+
+    if (!user) {
+      throw new ApiError(401, "Invalid refresh token")
+    }
+
+    if (incomingRefreshToken !== user?.refreshToken) {
+      throw new ApiError(401, "Invalid refresh token")
+    }
+
+    const options = {
+      httpOnly: true,
+      secure: true
+    }
+
+    const { accessToken, newrefreshToken } = await genarateAccessAndRefreshToken(user?._id)
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", newrefreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          {
+            accessToken, refreshToken: newrefreshToken
+          },
+          "Access token refreshed successfully"
+        )
+      )
+
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid refresh token")
+  }
+})
+
 export {
   registerUser,
   loginUser,
-
+  logoutUser
 }
