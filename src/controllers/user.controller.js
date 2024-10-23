12be91ffe4c3adc1f7d3099c7c7dcb13cfcee328
bappi12312 <ranamdbappihossain@gmail.com>
2024-10-23 12:58:11 +0600

    get user watch history

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -377,6 +377,58 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
   )
 })
 
+const getUserWatchHistory = asyncHandler(async(req,res) => {
+ const user = await User.aggregate([
+  {
+    $match: {
+      _id: new mongoose.Types.ObjectId(req.user?._id)
+    }
+  },
+  {
+    $lookup: {
+      from: "videos",
+      localField: "watchHistory",
+      foreignField: "_id",
+      as: "watchHistory",
+      pipeline: [
+        {
+          $lookup: {
+            from: "users",
+            localField: "owner",
+            foreignField: "_id",
+            as: "owner",
+            pipeline:[
+              {
+                $project:{
+                  fullName: 1,
+                  username: 1,
+                  avatar: 1
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  },
+  {
+    $addFields: {
+      owner: {
+        $first: "$owner",
+      }
+    }
+  }
+
+ ])
+
+ return res
+.status(200)
+.json(
+  new ApiResponse(200,user[0].watchHistory,"Watch History fetched successfully")
+)
+})
+
+
 
 
 export {
@@ -389,5 +441,6 @@ export {
   updateUserAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
-  getUserChannelProfile
+  getUserChannelProfile,
+  getUserWatchHistory
 }
