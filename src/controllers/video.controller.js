0be64884dfbc74e30e2658e59cd3fcdebd860ec1
bappi12312 <ranamdbappihossain@gmail.com>
2024-11-02 21:33:38 +0600

    get single video and update video and delete video controllers added

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -142,3 +142,65 @@ const getSingleVideoById = asyncHandler(async (req, res) => {
   return res.status(200).json(new ApiResponse(200, video, "video found successfully"))
 })
 
+const updateVido = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+
+  const { title, description } = req.body;
+
+  if (!isValidObjectId(videoId)) throw new ApiError(400, "videoId not valid")
+
+  const video = await Video.findById(videoId)
+
+  if (!video && !(toString(video.owner) === toString(req.user?._id))) {
+    throw new ApiError(402, "video noto found")
+  }
+
+  if (!title || !description)
+    throw new ApiError(400, "title or description required")
+
+  // const thumbnailLocalPath = req.file?.path;
+  const thumbnailLocalPath = req.files.thumbnail[0].path;
+
+  if (!thumbnailLocalPath)
+    throw new ApiError(400, "thumbnail not found")
+
+  const thumbnail = await uploadOnCloudinary(thumbnailLocalPath)
+
+  if (!thumbnail)
+    throw new ApiError(400, "thumbnail not uploaded")
+
+
+  const updateVideo = await Video.findByIdAndUpdate(
+    videoId,
+    {
+      $set: {
+        title,
+        description,
+        thumbnail: thumbnail?.url,
+      }
+    },
+    { new: true }
+  )
+
+  if (!updateVideo)
+    throw new ApiError(400, "video not updated")
+
+  return res.status(200).json(new ApiResponse(200, updateVideo, "video updated successfully"))
+})
+
+const deleteVideo = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+
+
+  if (!isValidObjectId(videoId))
+    throw new ApiError(400, "video id is required")
+
+  const deleteVideo = await Video.findByIdAndDelete(videoId)
+
+  if (!deleteVideo)
+    throw new ApiError(400, "video not deleted")
+  return res.status(200).json(new ApiResponse(200, deleteVideo, "Video deleted successfully"))
+
+})
+
+
