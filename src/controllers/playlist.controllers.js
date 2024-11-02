f02f0967646b51a9118f53181b2108e778e781d2
bappi12312 <ranamdbappihossain@gmail.com>
2024-11-02 22:31:42 +0600

    get user playlist

--- src/controllers/playlist.controllers.js
+++ src/controllers/playlist.controllers.js
@@ -28,3 +28,68 @@ const createPlaylist = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(201, playlist, "Playlist Created Successfully"))
 })
+
+const getUserPlaylist = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid userId!");
+  }
+
+  const user = await User.findById(userId)
+  if (!user) {
+    throw new ApiError(404, "User not found!");
+  }
+
+  const playlists = await Playlist.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId)
+      }
+    },
+    {
+      $lookup: {
+        from: 'videos',
+        localField: "videos",
+        foreignField: "_id",
+        as: 'videos',
+        pipeline: [
+          {
+            $sort: { createdAt: -1 }
+          },
+          { $limit: 1 },
+          {
+            $project: {
+              thumbnail: 1,
+            }
+          }
+        ]
+      }
+    },
+    {
+      $addFields: {
+        playlistThumbnail: {
+          $cond: {
+            if: { $isArray: "$videos" },
+            then: { $first: "$videos.thumbnail" },
+            else: null,
+          }
+        },
+      }
+    },
+    {
+      $project: {
+        name: 1,
+        description: 1,
+        playlistThumbnail: 1
+      }
+    }
+  ])
+
+
+  return res.status(200).json(new ApiResponse(
+    200,
+    { playlists },
+    "Playlists fetched successfully"
+  ));
+})
