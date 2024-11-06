4d2e2a5e02a081e44d94fb0d3032d82a63a11a81
bappi12312 <ranamdbappihossain@gmail.com>
2024-11-06 21:27:31 +0600

    getUserTweets

--- src/controllers/tweet.controllers.js
+++ src/controllers/tweet.controllers.js
@@ -1,7 +1,7 @@
 import { ApiError } from "../utils/ApiError";
 import { ApiResponse } from "../utils/ApiResponse";
 import { asyncHandler } from "../utils/asyncHandler";
-import mongoose, { isValidObjectId } from "mongoose"
+import mongoose, { isValidObjectId, mongo } from "mongoose"
 import { User } from "../models/user.model";
 import { Tweet } from "../models/tweet.model";
 
@@ -19,4 +19,28 @@ const createTweet = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Tweet not created")
 
   return res.status(200).json(new ApiResponse(200, newTweet, "Tweet created successfully"))
+})
+
+const getUserTweets = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+  if (!isValidObjectId(userId))
+    throw new ApiError(400, "invalid user id")
+
+  const tweets = await Tweet.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId)
+      }
+    }, {
+      $project: {
+        content: 1,
+        _id: 1
+      }
+    }
+  ])
+
+  if (tweets.length == 0)
+    return res.status(200).json(new ApiResponse(200, tweets, "No tweet was found"))
+
+  return res.status(200).json(new ApiResponse(200, tweets, "Tweets found successfully"))
 })
